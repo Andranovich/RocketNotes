33136dbcbbde9196f2111cb3b323c05c79b6d806
andranovich <rodrigo.oliveiradeandrade@gmail.com>
2023-06-16 16:00:35 -0300

    First Commit

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import { Container } from './styles';
+
+export function Details () {
+
+  /* Conteúdo da interface dentro do return */
+  return(
+    // Cada return retorna um único elemento, independente de quantos tenham dentro dele
+    <Container>
+      <h1>Hello World!</h1>
+      <span>Rodrigo Andrade</span>
+    </Container>
+  )
+}
