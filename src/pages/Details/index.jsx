21ca2c4e3bd24a3627b8fa5ea83d3eb912790c6b
andranovich <rodrigo.oliveiradeandrade@gmail.com>
2023-06-24 02:48:17 -0300

    second commit

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,10 @@
-import { Container } from './styles';
+import { Container, Links, Content } from './styles';
+
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
+import { Button } from '../../components/Button';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Details () {
 
@@ -6,8 +12,33 @@ export function Details () {
   return(
     // Cada return retorna um único elemento, independente de quantos tenham dentro dele
     <Container>
-      <h1>Hello World!</h1>
-      <span>Rodrigo Andrade</span>
+      <Header/>
+
+      <main>
+        <Content>      
+
+        <ButtonText title="Excluir nota"/>
+
+        <h1>Introdução ao React</h1>
+        
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, architecto, asperiores officia ullam iure optio nam temporibus ipsum voluptatum laboriosam illo qui autem aut vel modi, ad exercitationem blanditiis eveniet.</p>
+
+        <Section title="Links úteis">
+          <Links>
+            <li><a href="#">Link 1</a></li>
+          </Links>
+        </Section>
+
+        <Section title="Marcadores">
+          <Tag title="express"/>
+          <Tag title="nodejs"/>        
+        </Section>
+
+
+        <Button title="Voltar"/>
+
+        </Content>
+      </main>
     </Container>
   )
 }
